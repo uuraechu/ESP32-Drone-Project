968b4c0719d7a675e695dc2eb74aeb38c3ef428e
Ugochukwu Uraechu <67017981+uuraechu@users.noreply.github.com>
2026-01-30 20:17:42 -0600

    Stopped Tracking Files

--- Motor_Radio_Control_Combined_v2/build/core/ColorFormat.c.d
+++
@@ -1,3 +0,0 @@
-c:\Users\uurae\Documents\ESP32-Drone-Project\Motor_Radio_Control_Combined_v2\build\core\ColorFormat.c.o: \
- C:\Users\uurae\AppData\Local\Arduino15\packages\esp32\hardware\esp32\3.3.5\cores\esp32\ColorFormat.c \
- C:\Users\uurae\AppData\Local\Arduino15\packages\esp32\hardware\esp32\3.3.5\cores\esp32\ColorFormat.h
