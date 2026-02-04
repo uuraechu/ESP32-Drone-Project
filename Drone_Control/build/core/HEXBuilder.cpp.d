bc706c359b5d354b1ebb1c39f25d835f9f7d550e
Ugochukwu Uraechu <67017981+uuraechu@users.noreply.github.com>
2026-02-03 23:41:36 -0600

    Changed File Names

---
+++ Drone_Control/build/core/HEXBuilder.cpp.d
@@ -0,0 +1,10 @@
+C:\Users\uurae\Documents\ESP32-Drone-Project\Motor_Radio_Control_Combined_v2\build\core\HEXBuilder.cpp.o: \
+ C:\Users\uurae\AppData\Local\Arduino15\packages\esp32\hardware\esp32\3.3.5\cores\esp32\HEXBuilder.cpp \
+ C:\Users\uurae\AppData\Local\Arduino15\packages\esp32\hardware\esp32\3.3.5\cores\esp32\HEXBuilder.h \
+ C:\Users\uurae\AppData\Local\Arduino15\packages\esp32\hardware\esp32\3.3.5\cores\esp32/WString.h \
+ C:\Users\uurae\AppData\Local\Arduino15\packages\esp32\hardware\esp32\3.3.5\cores\esp32/pgmspace.h \
+ C:\Users\uurae\AppData\Local\Arduino15\packages\esp32\hardware\esp32\3.3.5\cores\esp32/Stream.h \
+ C:\Users\uurae\AppData\Local\Arduino15\packages\esp32\hardware\esp32\3.3.5\cores\esp32/Print.h \
+ C:\Users\uurae\AppData\Local\Arduino15\packages\esp32\tools\esp32-arduino-libs\idf-release_v5.5-9bb7aa84-v2\esp32/include/newlib/platform_include/stdio.h \
+ C:\Users\uurae\AppData\Local\Arduino15\packages\esp32\hardware\esp32\3.3.5\cores\esp32/WString.h \
+ C:\Users\uurae\AppData\Local\Arduino15\packages\esp32\hardware\esp32\3.3.5\cores\esp32/Printable.h
